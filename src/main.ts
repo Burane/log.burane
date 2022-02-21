eaeac54ff05c6c1a39e0fa5ad7e14be89446e9d4
Antoine <kerbiriouAntoine@gmail.com>
2022-02-21 11:08:35 +0100

    validator & exeception handling

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { PrismaClientExceptionFilter } from './prisma-client-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // binds ValidationPipe to the entire application
+  app.useGlobalPipes(new ValidationPipe());
+
+  // apply transform to all responses
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  // 👇 apply PrismaClientExceptionFilter to entire application, requires HttpAdapterHost because it extends BaseExceptionFilter
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
+
   await app.listen(3000);
 }
 bootstrap();
