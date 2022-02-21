300a6d76a8ac97083d4857ea435948c1c2eb6921
Antoine <kerbiriouAntoine@gmail.com>
2022-02-21 10:37:38 +0100

    seed db

---
+++ prisma/seed/seed.ts
@@ -0,0 +1,19 @@
+import { PrismaClient } from '@prisma/client';
+import { users } from './users';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  for (const user of users) {
+    await prisma.user.create({ data: user });
+  }
+}
+
+main()
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
---
+++ prisma/seed/users.ts
@@ -0,0 +1,6 @@
+export const users = [
+  {
+    email: 'test@test.test',
+    password: 'test',
+  },
+];
