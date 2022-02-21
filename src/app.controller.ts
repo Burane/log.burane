7e54acb026080bc83086638a0049551c64745d88
Antoine <kerbiriouAntoine@gmail.com>
2022-02-21 11:08:00 +0100

    get users test route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private prisma: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('users')
+  findUsers() {
+    return this.prisma.user.findMany();
   }
 }
