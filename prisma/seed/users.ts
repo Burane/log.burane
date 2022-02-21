520964656fd5dfede2bad070321e667963bb4cf6
Antoine <kerbiriouAntoine@gmail.com>
2022-02-21 12:48:23 +0100

    seed user with hashed pass

--- prisma/seed/users.ts
+++ prisma/seed/users.ts
@@ -1,6 +1,6 @@
 export const users = [
   {
-    email: 'test@test.test',
-    password: 'test',
+    email: 'test@test.com',
+    password: '$2b$10$kBsm9PcfG7rMNDqjxR9jXeXdFuvruDPie3uKTOIMXYjvbuCTm8r9C', // 12456
   },
 ];
