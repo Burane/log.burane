803130db578c2a2e96e83db3d59894d993575e36
Antoine <kerbiriouAntoine@gmail.com>
2022-02-22 11:09:13 +0100

    add roles guards

--- prisma/seed/users.ts
+++ prisma/seed/users.ts
@@ -1,6 +1,9 @@
+import { Role } from '@prisma/client';
+
 export const users = [
   {
-    email: 'test@test.com',
+    email: 'admin@test.com',
     password: '$2b$10$kBsm9PcfG7rMNDqjxR9jXeXdFuvruDPie3uKTOIMXYjvbuCTm8r9C', // 12456
+    role: Role.SUPERADMIN,
   },
 ];
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,16 +4,19 @@ import { AuthController } from './auth.controller';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import * as Process from 'process';
+import { JwtGuard } from './guards/jwt.guard';
+import { JwtStrategy } from './guards/jwt.strategy';
+import { RolesGuard } from './guards/roles.guard';
 
 @Module({
   imports: [
     PassportModule,
     JwtModule.register({
       secret: Process.env.JWT_SECRET_KEY,
-      signOptions: { expiresIn: '60s' },
+      signOptions: { expiresIn: '3600s' },
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtGuard, JwtStrategy, RolesGuard],
 })
 export class AuthModule {}
