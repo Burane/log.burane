4c882c18cd7cfddcce593bfc4ffc35157bad2112
Antoine <kerbiriouAntoine@gmail.com>
2022-03-09 14:01:27 +0100

    apiPropertie annotation for swagger on dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,21 +1,26 @@
 import { IsEmail, IsEnum, IsString, Matches } from 'class-validator';
 import { Match } from '../../utils/decorator/match.decorator';
 import { Role } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message: 'Password is too weak',
   })
   password: string;
 
+  @ApiProperty()
   @IsString()
   @Match('password', { message: "Passwords don't match each other" })
   passwordConfirmation: string;
 
+  @ApiProperty()
   @IsEnum(Role, { message: 'Role is not valid' })
   role?: Role;
 }
--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -1,17 +1,21 @@
 import { IsEmail, IsEnum, IsString, Matches } from 'class-validator';
 import { Match } from '../../utils/decorator/match.decorator';
 import { Role } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateUserDto {
+  @ApiProperty()
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsString()
   @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
     message: 'Password is too weak',
   })
   password: string;
 
+  @ApiProperty()
   @IsString()
   @Match('password', { message: "Passwords don't match each other" })
   passwordConfirmation: string;
