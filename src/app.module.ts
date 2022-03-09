fa98dbd6b2fb536ac6995f9b4d3467d4a3f7c60d
Antoine <kerbiriouAntoine@gmail.com>
2022-03-09 17:02:05 +0100

    add mailer module TODO creater mailer service && templayte for mail

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,30 @@ import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
 import { CanCUDUserGuard } from './user/guards/canCUDUser.guard';
+import { MailerModule } from '@nestjs-modules/mailer';
+import * as path from 'path';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 @Module({
-  imports: [PrismaModule, AuthModule, ConfigModule.forRoot(), UserModule],
+  imports: [
+    PrismaModule,
+    AuthModule,
+    ConfigModule.forRoot(),
+    UserModule,
+    MailerModule.forRoot({
+      transport: 'smtps://user@domain.com:pass@smtp.domain.com',
+      defaults: {
+        from: '"nest-modules" <modules@nestjs.com>',
+      },
+      template: {
+        dir: path.join(__dirname, 'templates'),
+        adapter: new HandlebarsAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, CanCUDUserGuard],
 })
