719bd15fb1ac3bc8c6925a6a1550c71eeb31c183
Antoine <kerbiriouAntoine@gmail.com>
2022-04-25 16:42:34 +0200

    loading

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Routes, Route, Link, BrowserRouter } from 'react-router-dom';
-import { MantineProvider } from '@mantine/core';
 import { HelloWorld } from './components/HelloWorld';
 import { Login } from './pages/Login';
 import { StoreProvider } from './providers/StoreProvider';
 import { RootStoreModel } from './stores/root.store';
 import { setupRootStore } from './stores/store.persistence';
+import { Center, Loader } from '@mantine/core';
 
 export const App = ({}) => {
   const [rootStore, setRootStore] = useState<RootStoreModel | undefined>(
@@ -18,24 +18,20 @@ export const App = ({}) => {
     })();
   }, []);
 
-  // const { userStore } = useStore();
-  //
-  // useEffect(() => {
-  //   userStore.getUser();
-  // }, []);
-
   if (!rootStore) {
-    return <div>Loading...</div>;
+    return (
+      <Center style={{ height: '100vh' }}>
+        <Loader color="grape" size="xl" variant="bars" />
+      </Center>
+    );
   }
   return (
     <StoreProvider value={rootStore}>
       <BrowserRouter>
-        <MantineProvider>
-          <Routes>
-            <Route path="/" element={<HelloWorld />} />
-            <Route path="/login" element={<Login />} />
-          </Routes>
-        </MantineProvider>
+        <Routes>
+          <Route path="/" element={<HelloWorld />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
       </BrowserRouter>
     </StoreProvider>
   );
