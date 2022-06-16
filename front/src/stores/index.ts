ebbb8ac7c8e1498d456911b2b4cef0111dc33ae7
Antoine <kerbiriouAntoine@gmail.com>
2022-06-16 16:21:48 +0200

    fix errors

--- front/src/stores/index.ts
+++ front/src/stores/index.ts
@@ -1,20 +1,24 @@
 import { RootStore, RootStoreEnv, RootStoreModel } from './root.store';
 import { UserStoreModel } from './user/user.store';
 import { AuthStoreModel } from './auth/auth.store';
+import { LogStoreModel } from './log/log.store';
+import { ApplicationStoreModel } from './application/application.store';
 
 export const createStore = (): RootStoreModel => {
   const userStore = UserStoreModel.create();
   const authStore = AuthStoreModel.create();
+  const appStore = ApplicationStoreModel.create();
+  const logStore = LogStoreModel.create();
 
-  const env: RootStoreEnv = { userStore, authStore };
+  const env: RootStoreEnv = { userStore, authStore, appStore, logStore };
 
-  const rootStore = RootStore.create(
+  return RootStore.create(
     {
       userStore,
       authStore,
+      appStore,
+      logStore,
     },
     env,
   );
-
-  return rootStore;
 };
